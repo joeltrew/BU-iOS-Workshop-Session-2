df4ce2090de2eccdc09367011ad81af17d28b6a6
Joel Trew <joel@trew.org.uk>
2015-02-12 09:41:13 +0000

    Initial commit, craeted table view with controller

--- Session2/CheckListViewController.swift
+++ Session2/CheckListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CheckListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CheckItem") as UITableViewCell
+        
+        return cell
+    }
+    
 }
+
 
